83e9fe0f4f07cd82ebc07be549a0584671f5f9db
kalombo <nogamemorebrain@gmail.com>
2018-03-21 12:51:05 +0500

    Bump version: 0.1.9 → 0.1.10

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 if __name__ == '__main__':
     setup(name='txoids',
-          version='0.1.9',
+          version='0.1.10',
           url="https://github.com/kalombos/txoids",
           download_url="https://github.com/kalombos/txoids",
           description='Tool for parsing switches using SNMP',
